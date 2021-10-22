1c6681565969d1ec22c7cee8e02ef9fbbbc87bb7
Joshua Grafton <grafton.josh@gmail.com>
2021-10-22 14:33:51 -0600

    completed video player with class components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import SearchBar from './SearchBar'
+import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
 import youtube from '../apis/youtube'
 
 
 class App extends React.Component{
 
     state = {
-        videos: []
+        videos: [],
+        selectedVideo: null
     }
 
     onTermSubmit = async (term) => {
@@ -17,13 +20,41 @@ class App extends React.Component{
             }
         })
         console.log(response);
-        this.setState({ videos: response.data.items})
+        this.setState({ 
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        })
+    }
+
+    onVideoSelect = (video) => {
+        console.log('<<< FROM APP', video);
+        this.setState({
+            selectedVideo: video
+        })
+    }
+
+    componentDidMount() {
+        this.onTermSubmit('4Runner')
     }
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                I have {this.state.videos.length} videos.
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail
+                                video={this.state.selectedVideo}
+                            />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList
+                                videos={this.state.videos}
+                                onVideoSelect={this.onVideoSelect}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
